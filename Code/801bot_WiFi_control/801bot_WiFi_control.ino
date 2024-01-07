056937b7c270df63b760624f81b41fadb7055027
Courtney Nguyen <hello@courtyen.io>
2024-01-06 22:35:25 -0700

    Push color LED robot code

--- Code/801bot_WiFi_control/801bot_WiFi_control.ino
+++ Code/801bot_WiFi_control/801bot_WiFi_control.ino
@@ -6,14 +6,19 @@
 #include "src/ESP32-ESP32S2-AnalogWrite/Servo.h"
 
 #include "html.h"
+#include <Bonezegei_WS2812.h>
+
+#define LED_COUNT 2
+Bonezegei_WS2812 rgb(38, LED_COUNT);
 
 
 // ------ Begin User Settings ------
-const char* ssid = "your-ssid";
-const char* password = "your-password";
+const char* ssid = "lanbeforetime";
+const char* password = "littlefoot";
 
 const int rServoPin = 33;
 const int lServoPin = 34;
+const int eyesPin = 38;
 
 const int rDeadzoneVal = 1500;
 const int lDeadzoneVal = 1500;
@@ -43,7 +48,13 @@ void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventTyp
 }
 
 void setup() {
+  // Not sure what this does
   Serial.begin(115200);
+
+  // Color stuff
+  rgb.begin();
+
+  // Servo stuff
   Serial.print("Begin");
 
   servo.attach(lServoPin);
@@ -65,9 +76,6 @@ void setup() {
   
   server.begin();
 
-}
-
-void loop() {
 }
 
 void updateServos() {
@@ -93,3 +101,133 @@ void updateServos() {
     servo.pause();
   }
 }
+
+/*
+  Strip RGB LED Demo WS2812 LED 
+  Author: Bonezegei (Jofel Batutay)
+  Date: October 2023 
+  Uses 24 bit Hex color format
+  Note : The Colors used on this demo was not set to maximum since it will be too bright during testing
+*/
+
+void loop() {
+  rgb.fill(0x0f0000);  //RED
+  delay(1000);
+  rgb.fill(0x0f00);  //GREEN
+  delay(1000);
+  rgb.fill(0x0f);  //BLUE
+  delay(1000);
+  rgb.fill(0x0);
+  delay(1000);
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, 0x0f0000);
+    delay(10);
+  }
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, 0x0f00);
+    delay(10);
+  }
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, 0x0f);
+    delay(10);
+  }
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, a << 16);
+    delay(20);
+  }
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, a << 8);
+    delay(20);
+  }
+
+  for (int a = 0; a < LED_COUNT; a++) {
+    rgb.setPixel(a, a);
+    delay(20);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill(a);  //BLUE
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill(a);  //BLUE
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill(a << 8);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill(a << 8);
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill(a);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill(a);
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill(a << 16);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill(a << 16);
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill((a << 16) | (a << 8));
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill((a << 16) | (a << 8));
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill((a << 8) | a);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill((a << 8) | a);
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill((a << 16) | a);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill((a << 16) | a);
+    delay(10);
+  }
+
+  //fade
+  for (int a = 0; a < 96; a++) {
+    rgb.fill((a << 8) | (a << 16) | a);
+    delay(10);
+  }
+  for (int a = 96; a > 0; a--) {
+    rgb.fill((a << 8) | (a << 16) | a);
+    delay(10);
+  }
+}
+
